e6adb98d7c3722632d6f0370d7d727e1a0009620
alexwnovak <alexwnovak@gmail.com>
2013-10-20 16:13:08 -0400

    +DI mapping for ArgumentParser.

--- Source/DirCommand/DirCommand/Program.cs
+++ Source/DirCommand/DirCommand/Program.cs
@@ -7,6 +7,7 @@
          Dependency.CreateUnityContainer();
          Dependency.RegisterType<IFileSystem, FileSystem>();
          Dependency.RegisterType<IConsoleAdapter, ConsoleAdapter>();
+         Dependency.RegisterType<IArgumentParser, ArgumentParser>();
       }
 
       private static void Main( string[] arguments )

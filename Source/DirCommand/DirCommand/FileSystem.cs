f62110c9042e8920e6d0be653553d5406f518a60
Alex Novak <alexwnovak@gmail.com>
2013-10-22 22:32:43 -0400

    +FileSystem implementation and DI mapping.

---
+++ Source/DirCommand/DirCommand/FileSystem.cs
@@ -0,0 +1,15 @@
+﻿namespace DirCommand
+{
+   public class FileSystem : IFileSystem
+   {
+      public string[] GetDirectories( string path )
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public string[] GetFiles( string path )
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
--- Source/DirCommand/DirCommand/Program.cs
+++ Source/DirCommand/DirCommand/Program.cs
@@ -5,10 +5,11 @@
       private static void InitDependencyInjection()
       {
          Dependency.CreateUnityContainer();
-         Dependency.RegisterType<IFileController, FileController>();
-         Dependency.RegisterType<IConsoleAdapter, ConsoleAdapter>();
          Dependency.RegisterType<IArgumentParser, ArgumentParser>();
+         Dependency.RegisterType<IFileController, FileController>();
+         Dependency.RegisterType<IFileSystem, FileSystem>();
          Dependency.RegisterType<IDisplayController, DisplayController>();
+         Dependency.RegisterType<IConsoleAdapter, ConsoleAdapter>();
       }
 
       private static int Main( string[] arguments )

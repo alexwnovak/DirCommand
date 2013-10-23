f3562175bc60069e6c030a49b5e0edb37b6eb380
Alex Novak <alexwnovak@gmail.com>
2013-10-22 21:24:30 -0400

    +Plumbing to connect arguments with methods that handle them.
    +Dictionary to map arguments to Actions.
    +These are discovered at runtime on the ArgumentHandlers class.
    +Handler methods must be decorated with the ArgumentHandlerAttribute class.

--- Source/DirCommand/DirCommand/ArgumentParser.cs
+++ Source/DirCommand/DirCommand/ArgumentParser.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace DirCommand
 {
    public class ArgumentParser : IArgumentParser
    {
+      private static readonly Dictionary<string, Action<RunSettings>> _argumentHandlers = DiscoverArgumentHandlers();
+
+      private static Dictionary<string, Action<RunSettings>> DiscoverArgumentHandlers()
+      {
+         var argumentHandlers = new Dictionary<string, Action<RunSettings>>();
+
+         var methods = typeof( ArgumentHandlers ).GetMethods( BindingFlags.Static | BindingFlags.Public );
+
+         foreach ( var method in methods )
+         {
+            var attributes = method.GetCustomAttributes( typeof( ArgumentHandlerAttribute ), false );
+
+            if ( attributes.Length == 0 )
+            {
+               // Not a handler--method is not decorated with attribute
+               continue;
+            }
+
+            string respondsTo = ((ArgumentHandlerAttribute) attributes[0]).Argument;
+
+            var action = (Action<RunSettings>) method.CreateDelegate( typeof( Action<RunSettings> ) );
+
+            argumentHandlers[respondsTo] = action;
+         }
+
+         return argumentHandlers;
+      }
+
       public RunSettings Parse( string[] arguments )
       {
          var runSettings = new RunSettings();
-         
+
          if ( arguments == null || arguments.Length == 0 )
          {
             return runSettings;
@@ -15,28 +45,29 @@ namespace DirCommand
 
          foreach ( string argument in arguments )
          {
-            if ( argument.StartsWith( "/" ) )
+            if ( IsProgramSwitch( argument ) )
             {
-               if ( argument.ToLower() == "/r" )
-               {
-                  runSettings.RecurseSubdirectories = true;
-               }
-               else if ( argument.ToLower() == "/l" )
-               {
-                  runSettings.DisplayAsLowercase = true;
-               }
-               else
+               Action<RunSettings> handler;
+
+               if ( !_argumentHandlers.TryGetValue( argument.ToLower(), out handler ) )
                {
                   throw new ArgumentException( "Unrecognized argument:" + argument );
                }
+
+               handler( runSettings );
             }
             else
             {
-               runSettings.Path = argument;               
+               runSettings.Path = argument;
             }
          }
 
          return runSettings;
+      }
+
+      private static bool IsProgramSwitch( string argument )
+      {
+         return argument.StartsWith( "/" );
       }
    }
 }

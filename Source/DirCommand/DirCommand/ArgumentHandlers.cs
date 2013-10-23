fa73e0b6211290c5b77d7edef524f9738fd520e0
Alex Novak <alexwnovak@gmail.com>
2013-10-22 21:17:00 -0400

    +ArgumentHandlers class to handle argument logic.

---
+++ Source/DirCommand/DirCommand/ArgumentHandlers.cs
@@ -0,0 +1,17 @@
+﻿namespace DirCommand
+{
+   internal static class ArgumentHandlers
+   {
+      [ArgumentHandler( "/l" )]
+      public static void ParseLowercaseFlag( RunSettings runSettings )
+      {
+         runSettings.DisplayAsLowercase = true;
+      }
+
+      [ArgumentHandler( "/r" )]
+      public static void ParseRecursionFlag( RunSettings runSettings )
+      {
+         runSettings.RecurseSubdirectories = true;
+      }
+   }
+}

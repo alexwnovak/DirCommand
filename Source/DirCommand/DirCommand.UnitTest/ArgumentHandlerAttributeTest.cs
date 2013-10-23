03bac2e8b542998a0e1beffc7e1a73c3626c746e
Alex Novak <alexwnovak@gmail.com>
2013-10-22 20:46:40 -0400

    +ArgumentHandlerAttribute and initial test.

---
+++ Source/DirCommand/DirCommand.UnitTest/ArgumentHandlerAttributeTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DirCommand.UnitTest
+{
+   [TestClass]
+   public class ArgumentHandlerAttributeTest
+   {
+      [TestMethod]
+      public void Constructor_PassesArgument_StoresArgument()
+      {
+         const string argument = "arg";
+
+         var argumentHandlerAttribute = new ArgumentHandlerAttribute( argument );
+
+         Assert.AreEqual( argument, argumentHandlerAttribute.Argument );
+      }
+   }
+}
---
+++ Source/DirCommand/DirCommand/ArgumentHandlerAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DirCommand
+{
+   [AttributeUsage( AttributeTargets.Method )]
+   public class ArgumentHandlerAttribute : Attribute
+   {
+      private readonly string _argument;
+
+      public string Argument
+      {
+         get
+         {
+            return _argument;
+         }
+      }
+
+      public ArgumentHandlerAttribute( string argument )
+      {
+         _argument = argument.ToLower();
+      }
+   }
+}

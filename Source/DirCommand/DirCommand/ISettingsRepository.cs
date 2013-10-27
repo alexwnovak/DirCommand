ebfb63d3e0f043b8ca4e6ad4148db4410bdaeecf
Alex Novak <alexwnovak@gmail.com>
2013-10-26 22:08:17 -0400

    +ISettingsRepository.

---
+++ Source/DirCommand/DirCommand/ISettingsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DirCommand
+{
+   public interface ISettingsRepository
+   {
+      Dictionary<string, ConsoleColor> GetFileColors();
+   }
+}

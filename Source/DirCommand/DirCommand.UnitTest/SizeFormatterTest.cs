9fe3825d5981fc063c6c1d8cfc89ecb5043b5d0c
Alex Novak <alexwnovak@gmail.com>
2013-10-27 20:57:39 -0400

    +SizeFormatter and initial test.

---
+++ Source/DirCommand/DirCommand.UnitTest/SizeFormatterTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DirCommand.UnitTest
+{
+   [TestClass]
+   public class SizeFormatterTest
+   {
+      [TestMethod]
+      public void GetSizeString_PassesZero_ReturnsCorrectString()
+      {
+         string sizeString = SizeFormatter.GetSizeString( 0 );
+
+         Assert.AreEqual( sizeString, "  0 B " );
+      }
+   }
+}
---
+++ Source/DirCommand/DirCommand/SizeFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace DirCommand
+{
+   public static class SizeFormatter
+   {
+      public static string GetSizeString( long bytes )
+      {
+         return "  0 B ";
+      }
+   }
+}

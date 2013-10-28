f2250d561b3b7b8371c2a06d02d61be770a26ebf
Alex Novak <alexwnovak@gmail.com>
2013-10-27 22:46:18 -0400

    Formatting.

--- Source/DirCommand/DirCommand/FileController.cs
+++ Source/DirCommand/DirCommand/FileController.cs
@@ -1,31 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
-using System.Linq;
 
 namespace DirCommand
 {
    public class FileController : IFileController
    {
-      //public FileEntry[] GetFiles( string path )
-      //{
-      //   //var fileSystem = Dependency.Resolve<IFileSystem>();
-
-      //   //return fileSystem.GetFiles( path ).Select( f => new FileEntry
-      //   //{
-      //   //   FullName = f
-      //   //}).ToArray();
-
-      //   //var fileSystem = Dependency.Resolve<IFileSystem>();
-
-      //   //var directories = fileSystem.GetDirectories( path ).Select( Path.GetFileName );
-
-      //   //var files = fileSystem.GetFiles( path ).Select( Path.GetFileName );
-
-      //   //return directories.Concat( files ).ToArray();
-      //}
-
       public void Run( RunSettings runSettings )
       {
          var fileSystem = Dependency.Resolve<IFileSystem>();

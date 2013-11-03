d311bd1774f0eec159c29c1537c2f26aae3aebed
Alex Novak <alexwnovak@gmail.com>
2013-11-03 12:02:43 -0500

    Fixing up unit tests that now depend on DisplayController.
    Moved display-related tests into DisplayControllerTest file.
    Pasted display-related smarts into DisplayController (previously FileController).
    FileController is now super simple.

---
+++ Source/DirCommand/DirCommand.UnitTest/DisplayControllerTest.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DirCommand.UnitTest
+{
+   [TestClass]
+   public class DisplayControllerTest
+   {
+      //[TestMethod]
+      //public void Run_PathContainsExecutable_DisplaysExecutableWithColor()
+      //{
+      //   const ConsoleColor exeColor = ConsoleColor.Green;
+      //   const string fileName = "SomeExecutable.exe";
+      //   var fileEntries = TestHelper.GetFileEntries( fileName );
+
+      //   // Setup
+
+      //   var fileSystemMock = new Mock<IFileSystem>();
+      //   fileSystemMock.Setup( fs => fs.GetFiles( It.IsAny<string>() ) ).Returns( fileEntries );
+      //   Dependency.RegisterInstance( fileSystemMock.Object );
+
+      //   var consoleAdapterMock = new Mock<IConsoleAdapter>();
+      //   Dependency.RegisterInstance( consoleAdapterMock.Object );
+
+      //   var settingsRepoMock = new Mock<ISettingsRepository>();
+      //   settingsRepoMock.Setup( sr => sr.GetExtensionColor( ".exe" ) ).Returns( exeColor );
+      //   Dependency.RegisterInstance( settingsRepoMock.Object );
+
+      //   // Test
+
+      //   var fileController = new FileController();
+
+      //   fileController.Run( new RunSettings() );
+
+      //   // Assert
+
+      //   consoleAdapterMock.Verify( ca => ca.WriteLine( fileName, ConsoleColor.Green ), Times.Once() );
+      //}
+
+      //[TestMethod]
+      //public void Run_PathContainsUppercaseFile_DisplaysProperColor()
+      //{
+      //   const ConsoleColor exeColor = ConsoleColor.Green;
+      //   const string fileName = "SomeExecutable.EXE";
+      //   var fileEntries = TestHelper.GetFileEntries( fileName );
+
+      //   // Setup
+
+      //   var fileSystemMock = new Mock<IFileSystem>();
+      //   fileSystemMock.Setup( fs => fs.GetFiles( It.IsAny<string>() ) ).Returns( fileEntries );
+      //   Dependency.RegisterInstance( fileSystemMock.Object );
+
+      //   var consoleAdapterMock = new Mock<IConsoleAdapter>();
+      //   Dependency.RegisterInstance( consoleAdapterMock.Object );
+
+      //   var settingsRepoMock = new Mock<ISettingsRepository>();
+      //   settingsRepoMock.Setup( sr => sr.GetExtensionColor( ".exe" ) ).Returns( exeColor );
+      //   Dependency.RegisterInstance( settingsRepoMock.Object );
+
+      //   // Test
+
+      //   var fileController = new FileController();
+
+      //   fileController.Run( new RunSettings() );
+
+      //   // Assert
+
+      //   consoleAdapterMock.Verify( ca => ca.WriteLine( fileName, ConsoleColor.Green ), Times.Once() );
+      //}
+
+      //[TestMethod]
+      //public void Run_HasFileLength_LengthIsWrittenToConsole()
+      //{
+      //   var fileEntry = new FileEntry
+      //   {
+      //      FullName = "SomeFile.txt",
+      //      Length = 123456789
+      //   };
+
+      //   string expectedLengthString = SizeFormatter.GetSizeString( fileEntry.Length ) + " ";
+
+      //   // Setup
+
+      //   var fileSystemMock = new Mock<IFileSystem>();
+      //   fileSystemMock.Setup( fs => fs.GetFiles( It.IsAny<string>() ) ).Returns( fileEntry.AsArray() );
+      //   Dependency.RegisterInstance( fileSystemMock.Object );
+
+      //   var settingsRepoMock = new Mock<ISettingsRepository>();
+      //   Dependency.RegisterInstance( settingsRepoMock.Object );
+
+      //   var consoleAdapterMock = new Mock<IConsoleAdapter>();
+      //   Dependency.RegisterInstance( consoleAdapterMock.Object );
+
+      //   // Test
+
+      //   var fileController = new FileController();
+
+      //   fileController.Run( new RunSettings() );
+
+      //   // Assert
+
+      //   consoleAdapterMock.Verify( ca => ca.Write( expectedLengthString ), Times.Once() );
+      //}
+   }
+}
--- Source/DirCommand/DirCommand/DisplayController.cs
+++ Source/DirCommand/DirCommand/DisplayController.cs
@@ -1,9 +1,28 @@
-﻿namespace DirCommand
+﻿using System;
+using System.IO;
+
+namespace DirCommand
 {
    public class DisplayController : IDisplayController
    {
       public void Display( FileEntry[] files )
       {
+         //var settingsRepository = Dependency.Resolve<ISettingsRepository>();
+
+         //var consoleAdapter = Dependency.Resolve<IConsoleAdapter>();
+
+         //foreach ( var file in files )
+         //{
+         //   string sizeString = SizeFormatter.GetSizeString( file.Length );
+
+         //   consoleAdapter.Write( sizeString + " " );
+
+         //   string extension = Path.GetExtension( file.FullName ).ToLower();
+
+         //   ConsoleColor color = settingsRepository.GetExtensionColor( extension );
+
+         //   consoleAdapter.WriteLine( Path.GetFileName( file.FullName ), color );
+         //}
       }
 
       public void ShowError( string message )
